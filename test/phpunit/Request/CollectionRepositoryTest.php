1a408411d769bd83f8a3bd11e700f247f17ed01f
Greg Bowler <greg@g105b.com>
2026-06-05 10:14:57 +0100

    feature: fork collections closes #21

--- test/phpunit/Request/CollectionRepositoryTest.php
+++ test/phpunit/Request/CollectionRepositoryTest.php
@@ -3,6 +3,7 @@ namespace App\Test\Request;
 
 use App\Request\Collection\CollectionMode;
 use App\Request\Collection\PrivateCollectionRepository;
+use App\Request\PrivateRequestRepository;
 use PHPUnit\Framework\TestCase;
 
 class CollectionRepositoryTest extends TestCase {
@@ -38,6 +39,67 @@ class CollectionRepositoryTest extends TestCase {
 		);
 
 		self::assertNull($sut->getCurrent());
+	}
+
+	public function testFork_copiesRequestsWithoutResponsesOrSecrets():void {
+		$sourceRoot = "$this->tmpDir/source";
+		$targetRoot = "$this->tmpDir/target";
+		mkdir("$sourceRoot/request", recursive: true);
+		mkdir("$targetRoot/request", recursive: true);
+
+		$sourceCollectionRepository = new PrivateCollectionRepository($sourceRoot, CollectionMode::request);
+		$targetCollectionRepository = new PrivateCollectionRepository($targetRoot, CollectionMode::request);
+
+		$sourceCollection = $sourceCollectionRepository->create("Access Card");
+		$sourceCollectionRepository->save($sourceCollection);
+
+		$sourceRequestRepository = new PrivateRequestRepository(
+			"$sourceRoot/request/$sourceCollection->id",
+		);
+		$firstRequest = $sourceRequestRepository->create("Validate a card");
+		$firstRequest->endpoint = "https://example.com/validate";
+		$sourceRequestRepository->update($firstRequest);
+
+		$secondRequest = $sourceRequestRepository->create("Get a photo");
+		$secondRequest->endpoint = "https://example.com/photo";
+		$sourceRequestRepository->update($secondRequest);
+
+		mkdir("$sourceRoot/request/$sourceCollection->id/$firstRequest->id/response", recursive: true);
+		file_put_contents(
+			"$sourceRoot/request/$sourceCollection->id/$firstRequest->id/response/test.dat",
+			"response",
+		);
+		file_put_contents(
+			"$sourceRoot/request/$sourceCollection->id/secrets.ini",
+			"API_KEY=\"secret\"\n",
+		);
+
+		$forkedCollection = $targetCollectionRepository->fork(
+			$sourceCollectionRepository,
+			$sourceCollection,
+			"Forked Access Card",
+		);
+		$forkedRequestRepository = new PrivateRequestRepository(
+			"$targetRoot/request/$forkedCollection->id",
+		);
+
+		$forkedRequests = $forkedRequestRepository->retrieveAll();
+		self::assertSame("Forked Access Card", $forkedCollection->name);
+		self::assertCount(2, $forkedRequests);
+		self::assertSame(
+			[$firstRequest->id, $secondRequest->id],
+			array_map(fn($request) => $request->id, $forkedRequests),
+		);
+		self::assertSame(
+			[$firstRequest->endpoint, $secondRequest->endpoint],
+			array_map(fn($request) => $request->endpoint, $forkedRequests),
+		);
+		self::assertFileDoesNotExist(
+			"$targetRoot/request/$forkedCollection->id/secrets.ini",
+		);
+		self::assertDirectoryDoesNotExist(
+			"$targetRoot/request/$forkedCollection->id/$firstRequest->id/response",
+		);
 	}
 
 	private function deleteDir(string $dir):void {
