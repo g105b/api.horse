5a370c6dbfce6635b4b2e6a300c2b297bed48c89
Greg Bowler <greg@g105b.com>
2026-06-22 23:03:13 +0100

    tweak: force scheme

--- page/_component/request-editor.php
+++ page/_component/request-editor.php
@@ -126,6 +126,10 @@ function do_update(
 	$urlSuffix = "";
 
 	$endpointString = $input->getString("endpoint");
+	if(!str_contains($endpointString, "//")) {
+		$endpointString = "http://$endpointString";
+	}
+
 	if($queryString = parse_url($endpointString, PHP_URL_QUERY)) {
 		$endpointString = strtok($endpointString, "?");
 		parse_str($queryString, $queryParts);
